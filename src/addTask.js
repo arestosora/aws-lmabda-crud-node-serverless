becf8a3a21339a87d305511272e60ded5409283c
Ares <89419093+arestosora@users.noreply.github.com>
2024-06-16 19:02:20 -0500

    feat: add task

---
+++ src/addTask.js
@@ -0,0 +1,28 @@
+const { v4 } = require("uuid")
+const AWS = require("aws-sdk");
+
+const addTask = async (event) => {
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+    const { title, description } = JSON.parse(event.body);
+    const createdAt = new Date()
+    const id = v4();
+
+    const newTask = {
+        id,
+        title,
+        description,
+        createdAt
+    }
+
+    await dynamo.put({
+        TableName: 'TaskTable',
+        Item: newTask
+    }).promise()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(newTask)
+    }
+}
+
+module.exports = { addTask }
